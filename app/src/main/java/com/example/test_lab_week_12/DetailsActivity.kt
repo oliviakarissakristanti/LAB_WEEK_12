a0400e4b49b36652a909f9869da895bc0a0ea5dd
oliviakarissakristanti <koliviakarissa@gmail.com>
2025-12-01 00:35:36 +0700

    Commit No. 3 – Assignment

--- app/src/main/java/com/example/test_lab_week_12/DetailsActivity.kt
+++ app/src/main/java/com/example/test_lab_week_12/DetailsActivity.kt
@@ -13,24 +13,23 @@ class DetailsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
 
-        val posterView: ImageView = findViewById(R.id.details_poster)
-        val titleView: TextView = findViewById(R.id.details_title)
-        val dateView: TextView = findViewById(R.id.details_release_date)
-        val overviewView: TextView = findViewById(R.id.details_overview)
-        val voteView: TextView = findViewById(R.id.details_vote)
+        val poster: ImageView = findViewById(R.id.details_poster)
+        val title: TextView = findViewById(R.id.details_title)
+        val date: TextView = findViewById(R.id.details_release_date)
+        val rating: TextView = findViewById(R.id.details_vote)
+        val overview: TextView = findViewById(R.id.details_overview)
 
         val movie = intent.getParcelableExtra<Movie>("movie")
 
         movie?.let {
-            val imageUrl = "https://image.tmdb.org/t/p/w500/"
-            titleView.text = it.title ?: "No title"
-            dateView.text = it.releaseDate ?: "Unknown"
-            overviewView.text = it.overview ?: "No overview"
-            voteView.text = "Rating: ${it.voteAverage}"
-            Glide.with(this)
-                .load("${imageUrl}${it.posterPath}")
-                .placeholder(R.mipmap.ic_launcher)
-                .into(posterView)
+            val imageUrl = "https://image.tmdb.org/t/p/w500/${it.posterPath}"
+
+            Glide.with(this).load(imageUrl).into(poster)
+
+            title.text = it.title ?: "No Title"
+            date.text = "Release: ${it.releaseDate ?: "-"}"
+            rating.text = "Rating: ${it.voteAverage}"
+            overview.text = it.overview ?: "No overview available"
         }
     }
 }
